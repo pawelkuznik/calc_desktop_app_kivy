8fed25235d5baef69325dea92be6976f6ae87335
SG0312733 <pawel.kuznik.ctr@sabre.com>
2022-01-08 13:44:49 +0100

    add inserting and deleting signs/digits into text area

--- calculator.py
+++ calculator.py
@@ -6,9 +6,29 @@ from kivy.lang.builder import Builder
 
 Builder.load_file('./calculator.kv')
 Window.size = (350, 550)
-class CalculatorWidget(Widget):
-    pass
 
+
+class CalculatorWidget(Widget):
+    def clear(self):
+        self.ids.input_box.text = '0'
+
+    def button_value(self, number):
+        prev_number = self.ids.input_box.text
+
+        if prev_number == '0':
+            self.ids.input_box.text = ''
+            self.ids.input_box.text = f"{number}"
+        else:
+            self.ids.input_box.text = f"{prev_number}{number}"
+
+    def signs(self, sign):
+        prev_number = self.ids.input_box.text
+        self.ids.input_box.text = f"{prev_number}{sign}"
+
+    def remove_last(self):
+        prev_number = self.ids.input_box.text
+        prev_number = prev_number[:-1]
+        self.ids.input_box.text = prev_number
 
 class CalculatorApp(App):
     def build(self):
